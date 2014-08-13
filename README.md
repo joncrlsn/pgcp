fbaa37304c30dea91e4ef813e473afb90d5e1b1b
Jon Carlson <jon@code42.com>
2014-08-13 17:15:35 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-pgcp
-====
+# pgcp - PostgreSQL table copy
+
+
+Written in GoLang, this utility copies some or all rows of a Postgres database table as INSERT or UPDATE statements.  All columns are included in the INSERT or UPDATE statements.
+
+A couple of binaries to save you the effort:
+[Mac](https://github.com/joncrlsn/pgrun/raw/master/bin-osx/pgcp "OSX version")  [Linux](https://github.com/joncrlsn/pgrun/raw/master/bin-linux/pgcp "Linux version")
+
+## usage
+
+	pgcp [database flags] <genType> <tableName> [idColumn] <whereClause>
+
+
+database flags | Explanation 
+-------------: | -------------
+  -U           | postgres user   (matches psql flag)
+  -h           | database host -- default is localhost (matches psql flag)
+  -p           | port.  defaults to 5432 (matches psql flag)
+  -d           | database name (matches psql flag)
+  -pw          | password for the postgres user<br>(if not provided then you'll be prompted)
+
+
+Argument            | Explanation 
+--------:           | -------------
+&lt;genType&gt;     | type of SQL to generate: INSERT or UPDATE.<br/>(case insensitive)
+&lt;tableName&gt;   | name of table to be outputted (fully or partially)
+\[idColumn\]        | only used when genType is "update"
+&lt;whereClause&gt; | specifies which rows to copy.  example:<br> "WHERE user_id < 100 AND username IS NOT NULL"
+
+#### Database connection information can be specified in up to three ways:
+
+  * Environment variables (keeps you from typing them in often)
+  * Program flags (overrides environment variables.  See above)
+  * ~/.pgpass file (may contain password for the previously specified user)
+  * Note that if password is not specified, you will be prompted.
+
+#### Optional database environment variables
+
+Name   | Explanation
+-----  | -----------
+DBHOST | host name where database is running (default is localhost)
+DBPORT | port database is listening on (default is 5432)
+DBNAME | name of database you want to copy
+DBUSER | user in postgres you'll be executing the queries as
+DBPASS | password for the user
+DBOPTIONS | one or more database options (like sslmode=disable)
---
+++ pgcp.go
@@ -0,0 +1,236 @@
+package main
+
+import "fmt"
+
+//import "reflect"
+import "time"
+import "log"
+import "flag"
+import "os"
+import "strings"
+import "database/sql"
+import _ "github.com/lib/pq"
+import "github.com/joncrlsn/pgutil"
+import "github.com/joncrlsn/misc"
+
+const isoFormat = "2006-01-02T15:04:05.000-0700"
+
+/*
+ * Queries the given table name and copies the column values to either an INSERT statement or
+ * an UPDATE statement.
+ *
+ * Example: pgcp -U myuser -d mydb INSERT t_user "WHERE user_id < 4"
+ */
+func main() {
+	dbInfo := pgutil.DbInfo{}
+	dbInfo.Populate()
+	//fmt.Println("dbInfo:", dbInfo)
+	//fmt.Println("connection string:", dbInfo.ConnectionString())
+
+	// Remaining args:
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, "Missing some arguments")
+		usage()
+	}
+
+	// genType
+	genType := strings.ToUpper(args[0])
+	if genType != "INSERT" && genType != "UPDATE" {
+		fmt.Fprintf(os.Stderr, "Invalid generation type: %s.  Requires either INSERT or UPDATE\n", genType)
+		usage()
+	}
+
+	// tableName
+	tableName := args[1]
+
+	// idColumn (UPDATE only) and whereClause
+	whereClause := ""
+	idCol := ""
+	if genType == "INSERT" {
+		if len(args) > 2 {
+			whereClause = args[2]
+		}
+	} else {
+		if len(args) < 3 {
+			fmt.Fprintf(os.Stderr, "UPDATE requires an idColumn.")
+			usage()
+		}
+		idCol = args[2]
+		if len(args) > 3 {
+			whereClause = args[3]
+		}
+	}
+
+	if len(whereClause) == 0 {
+		// Make sure user intended there to be no where clause
+		if !misc.PromptYesNo("Did you intend to have no where clause?", true) {
+			os.Exit(1)
+		}
+	}
+
+	db, err := dbInfo.Open()
+	check("opening database", err)
+
+	query := "SELECT * FROM " + tableName + " " + whereClause
+	fmt.Println("-- Running query:", query)
+	rowChan, columnNames := querySqlValues(db, query)
+
+	for row := range rowChan {
+		if genType == "INSERT" {
+			generateInsert(tableName, row, columnNames)
+		} else {
+			generateUpdate(tableName, row, idCol)
+		}
+	}
+}
+
+func generateInsert(tableName string, row map[string]string, colNames []string) {
+	fmt.Printf("INSERT INTO %s (", tableName)
+	first := true
+	for _, name := range colNames {
+		//fmt.Println("ix/name:", ix, name)
+		if !first {
+			fmt.Print(", ")
+		}
+		fmt.Printf(name)
+		first = false
+	}
+	fmt.Print(") VALUES (")
+	first = true
+	for _, name := range colNames {
+		if !first {
+			fmt.Print(", ")
+		}
+		v := row[name]
+		fmt.Printf(v)
+		first = false
+	}
+	fmt.Println(");")
+}
+
+func generateUpdate(tableName string, row map[string]string, idCol string) {
+	fmt.Printf("UPDATE %s SET ", tableName)
+	idVal := ""
+	idColFound := false
+	first := true
+	for k, v := range row {
+		if k == idCol {
+			idVal = v
+			idColFound = true
+		} else {
+			if !first {
+				fmt.Print(", ")
+			}
+			fmt.Printf("%s=%s", k, v)
+			first = false
+		}
+	}
+	if !idColFound {
+		log.Fatalf("\nid column not found: %s\n", idCol)
+		os.Exit(1)
+	}
+	fmt.Printf(" WHERE %s=%s", idCol, idVal)
+	fmt.Println(";")
+}
+
+/*
+ * Returns row maps (keyed by the column name) in a channel.
+ * Dynamically converts each column value to a SQL string value.
+ * See http://stackoverflow.com/questions/23507531/is-golangs-sql-package-incapable-of-ad-hoc-exploratory-queries
+ */
+func querySqlValues(db *sql.DB, query string) (chan map[string]string, []string) {
+	rowChan := make(chan map[string]string)
+
+	rows, err := db.Query(query)
+	check("running query", err)
+	columnNames, err := rows.Columns()
+	check("getting column names", err)
+
+	go func() {
+
+		defer rows.Close()
+
+		vals := make([]interface{}, len(columnNames))
+		valPointers := make([]interface{}, len(columnNames))
+		// Copy
+		for i := 0; i < len(columnNames); i++ {
+			valPointers[i] = &vals[i]
+		}
+
+		for rows.Next() {
+			err = rows.Scan(valPointers...)
+			check("scanning a row", err)
+
+			row := make(map[string]string)
+			// Convert each cell to a SQL-valid string representation
+			for i, valPtr := range vals {
+				//fmt.Println(reflect.TypeOf(valPtr))
+				switch valueType := valPtr.(type) {
+				case nil:
+					row[columnNames[i]] = "null"
+				case []uint8:
+					row[columnNames[i]] = "'" + string(valPtr.([]byte)) + "'"
+				case string:
+					row[columnNames[i]] = "'" + valPtr.(string) + "'"
+				case int64:
+					row[columnNames[i]] = fmt.Sprintf("%d", valPtr)
+					//row[columnNames[i]] = string(valPtr.(int64))
+				case bool:
+					row[columnNames[i]] = fmt.Sprintf("%t", valPtr)
+				case time.Time:
+					row[columnNames[i]] = "'" + valPtr.(time.Time).Format(isoFormat) + "'"
+				case fmt.Stringer:
+					row[columnNames[i]] = fmt.Sprintf("%v", valPtr)
+				default:
+					row[columnNames[i]] = fmt.Sprintf("%v", valPtr)
+					fmt.Println("Column %s is an unhandled type: %v", columnNames[i], valueType)
+				}
+			}
+			rowChan <- row
+		}
+		close(rowChan)
+	}()
+	return rowChan, columnNames
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s [database flags] <genType> <tableName> <whereClause> \n", os.Args[0])
+	fmt.Fprintln(os.Stderr, `
+Copies table data as either INSERT or UPDATE statements.
+
+[database flags]: (optional)
+  -U     : postgres user (matches psql flag)
+  -h     : database host -- default is localhost (matches psql flag)
+  -p     : port.  defaults to 5432 (matches psql flag)
+  -d     : database name (matches psql flag)
+  -pw    : password for the postgres user (otherwise you'll be prompted)
+
+<genType>     : type of SQL to generate: insert, update
+
+<tableName>   : name of table to be copied (fully or partially)
+
+<whereClause> : specifies which rows to copy. Example: WHERE user_id < 100 AND username IS NOT NULL
+
+Database connection information can be specified in two ways:
+  * Environment variables
+  * Program flags (overrides environment variables.  See above)
+  * ~/.pgpass file (for the password)
+  * Note that if password is not specified, you will be prompted.
+
+Optional Environment variables (if program flags are not desirable):
+  DBHOST : host name where database is running (default is localhost)
+  DBPORT : port database is listening on (default is 5432)
+  DBNAME : name of database you want to copy
+  DBUSER : user in postgres you'll be executing the queries as
+  DBPASS : password for the user
+`)
+
+	os.Exit(2)
+}
+
+func check(msg string, err error) {
+	if err != nil {
+		log.Fatal("Error "+msg, err)
+	}
+}
